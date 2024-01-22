c6c299d25887e4c198c9e025e96a29e6d5f27714
bjugal <jugalbharmani98@gmail.com>
2024-01-22 20:18:47 +0530

    Modified Decorator classes to correctly unwrap when add_options is called

--- GlobalOptions.py
+++ GlobalOptions.py
@@ -1,20 +1,26 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 class GlobalOptionsBuilder(ABC):
     '''Abstract Component Class'''
-    _globalOptionsList:list=[]
-    
     @abstractmethod
     def add_options(self):
         pass
 
 class DefaultGlobalOptions(GlobalOptionsBuilder):
     '''Concrete Component Class'''
+    _globalOptionsList:List=None
+
+    @property
+    def globalOptionsList(self):
+        return self._globalOptionsList
+
     def __init__(self):
-        '''Initializing the _globalOptionsList declared in the Parent Class GlobalOptionsBuilder'''
+        '''Initializing the _globalOptionsList'''
         self._globalOptionsList=[]
+    
     def add_options(self):
-        return self
+        return self.globalOptionsList
 
 class GlobalOptionsDecorator(GlobalOptionsBuilder,ABC):
     '''Abstract Decorator class for adding multiple global options'''
@@ -24,46 +30,52 @@ class GlobalOptionsDecorator(GlobalOptionsBuilder,ABC):
         self._baseGlobalOptions = baseGlobalOptions    
 
     @property
-    def globalOptionsList(self):
-        return self._baseGlobalOptions._globalOptionsList
+    def baseGlobalOptions(self):
+        return self._baseGlobalOptions
     
-    @globalOptionsList.setter
-    def globalOptionsList(self,globalOptionsList):
-        self._baseGlobalOptions._globalOptionsList=globalOptionsList
+    def add_options(self):
+        pass
 
 class ConcreteHideFlagOptionDecorator(GlobalOptionsDecorator):
     '''Concrete Decorator to add hide flag option'''
-   
+
+    def __init__(self, baseGlobalOptions):
+        super().__init__(baseGlobalOptions)
+
     def add_options(self):
-        self.globalOptionsList=self.globalOptionsList+ ["-hide-flag"]
-        return self._baseGlobalOptions
+        return self.baseGlobalOptions.add_options() + ["-hide-flag"]
 
 class ConcreteOverwriteOutputFiles(GlobalOptionsDecorator):
     '''Concrete Decorator to to overwrite output files'''
 
+    def __init__(self, baseGlobalOptions, overwrite):
+        super().__init__(baseGlobalOptions)
+        self._overwrite=overwrite
+    
     def add_options(self,overwrite='n'):
-        self.globalOptionsList=self.globalOptionsList + ['-'+overwrite]
-        return self._baseGlobalOptions
+        return self.baseGlobalOptions.add_options() + ['-'+self._overwrite]
 
 class ConcreteProgressURL(GlobalOptionsDecorator):
     '''Concrete Decorator to tadd progress-url'''
+ 
+    def __init__(self, baseGlobalOptions,url):
+        super().__init__(baseGlobalOptions)
+        self._url=url
 
-    def add_options(self,url):
-        self.globalOptionsList=self.globalOptionsList + ['-progress '+url]
-        return self._baseGlobalOptions
+
+    def add_options(self):
+        return self.baseGlobalOptions.add_options() + ['-progress '+self._url]
     
 if __name__=='__main__':
+
     globalOptions=DefaultGlobalOptions()
-    # print(globalOptions._globalOptionsList)
-
-    globalOptions=ConcreteOverwriteOutputFiles(globalOptions)
-    globalOptions=globalOptions.add_options('y')
-    # print(globalOptions._globalOptionsList)
-
+    globalOptions=ConcreteOverwriteOutputFiles(globalOptions,'y')
     globalOptions=ConcreteHideFlagOptionDecorator(globalOptions)
-    globalOptions=globalOptions.add_options()
-    # print(globalOptions._globalOptionsList)
-
-    globalOptions=ConcreteProgressURL(globalOptions)
-    globalOptions=globalOptions.add_options('www.sample.com')
-    print(globalOptions._globalOptionsList)
+    
+    # Checking intermediate result
+    print(globalOptions.add_options())
+    globalOptions=ConcreteProgressURL(globalOptions,'www.sample.com')
+    
+    # Calling add_options to get the final list
+    fullGlobalOptionsList=globalOptions.add_options()
+    print(fullGlobalOptionsList)
